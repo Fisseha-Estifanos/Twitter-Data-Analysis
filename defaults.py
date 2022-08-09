af1382869077d66329b2fe8f3978acce2aa85ec4
fish <fisseha.137@gmail.com>
2022-08-09 12:17:59 +0300

    defaults: modified

--- defaults.py
+++ defaults.py
@@ -5,5 +5,12 @@ A script to store all default paths and strings.
 # the global data set
 global_data = 'data/global_twitter_data.json'
 
+# the processed global data set
+processed_global_data = 'data/processed_global_twitter_data.json'
+
+
 # the african data set
 african_data = 'data/africa_twitter_data.json'
+
+# the processed african data set
+processed_african_data = 'data/processed_africa_twitter_data.json'
