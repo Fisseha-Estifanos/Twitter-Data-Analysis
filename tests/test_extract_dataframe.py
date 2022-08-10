458265b76d5c830d0f00ff8eb91ad984e271652f
fish <fisseha.137@gmail.com>
2022-08-11 01:21:04 +0300

    path bug

--- tests/test_extract_dataframe.py
+++ tests/test_extract_dataframe.py
@@ -16,7 +16,7 @@ from extract_dataframe import TweetDfExtractor
 # Create a sample not more than 10 tweets and place it in a json file.
 # Provide the path to the samples tweets file you created below
 
-_, tweet_list = read_json(sample_data)
+tweet_list = pd.read_csv(sample_data)
 
 columns = [
     "created_at",
