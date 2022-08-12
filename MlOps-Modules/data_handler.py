c01517a6101dc94ccaddcf5c8cedb47879be59a3
fish <fisseha.137@gmail.com>
2022-08-13 01:10:36 +0300

    data base: query excutioner method created

--- MlOps-Modules/data_handler.py
+++ MlOps-Modules/data_handler.py
@@ -21,3 +21,32 @@ def DBConnect(dbName=None):
     conn = sqlite3.connect(dbName)
     print(conn)
     return conn
+
+
+def execute_query(connection: sqlite3.Connection, query:str) -> None:
+    """
+    A data base query executor method, based on a given connection string and a query string
+
+    Parameters
+    ----------
+    connection :
+        sqlite3.Connection : the database connection
+    query :
+        string : the query string
+    Returns :
+    -------
+    return : nothing
+    """
+
+    cursor = connection.cursor()
+    fd = open(query, 'r')
+    sql_query = fd.read()
+    fd.close()
+
+    try:
+        cursor.execute(sql_query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
